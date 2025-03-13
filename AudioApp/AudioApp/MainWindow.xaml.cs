73657dd35afc49c0a22df440b7dc68db1cd66f72
bart <bielski.bart1012@gmail.com>
2025-03-13 17:03:56 +0000

    FIX: Bind gain knob to audio engine - very basic data binding

--- AudioApp/AudioApp/MainWindow.xaml.cs
+++ AudioApp/AudioApp/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using AudioApp.Models;
 using NAudio.Wave.SampleProviders;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 
@@ -12,7 +11,7 @@ namespace AudioApp
         private HashSet<Key> keysHeld = new();
         private AudioEngine audioEngine;
         private SignalGeneratorType oscillatorOneType;
-        private float oscillatorOneGain;
+        private double oscillatorOneGain;
         private int oscillatorOneOctave;
 
         public MainWindow()
@@ -20,6 +19,11 @@ namespace AudioApp
             InitializeComponent();
             audioEngine = AudioEngine.GetInstance();
             audioEngine.Play();
+            audioEngine.SetOscillatorGain(0, 0.0);
+            OscOnePanel.GainChanged += (amount) =>
+            {
+                audioEngine.SetOscillatorGain(0, amount);
+            };
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -41,43 +45,7 @@ namespace AudioApp
 
         }
 
-        private void ComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (audioEngine is null) return;
-            if (sender is ComboBox cbox && cbox.SelectedItem != null)
-            {
-                string selected = cbox.SelectedItem.ToString().Split(": ").Last();
 
-                var waveformMap = new Dictionary<string, SignalGeneratorType>
-                {
-                    { "Sin", SignalGeneratorType.Sin },
-                    { "Triangle", SignalGeneratorType.Triangle },
-                    { "SawTooth", SignalGeneratorType.SawTooth },
-                    { "Square", SignalGeneratorType.Square }
-                };
-
-                audioEngine.SetOscillatorType(0, waveformMap[selected]);
-            }
-
-        }
-
-        private void VolumeSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (audioEngine is null) return;
-
-            var slider = sender as Slider;
-            float value = (float)slider.Value;
-            audioEngine.SetOscillatorGain(0, value);
-        }
-
-        private void OctaveSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (audioEngine is null) return;
-
-            var slider = sender as Slider;
-            int value = (int)slider.Value;
-            audioEngine.SetOscillatorOctave(0, value);
-        }
 
 
     }
